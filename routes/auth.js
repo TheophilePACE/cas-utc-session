c28b52865b5c7b447381f6f1feb47bb76e97dfd9
TheophilePACE <theophile.pace@gmail.com>
2018-04-15 19:55:24 +0200

    Refactoring the cookie-session part

--- routes/auth.js
+++ routes/auth.js
@@ -1,16 +1,31 @@
-const { requireLogin } = require('../utils/auth');
 const CASAuthentication = require('cas-authentication');
+const cookieSession = require('cookie-session')
+
+const { requireLogin } = require('../utils/auth');
+
+const { service_url, cas_url, cas_version } = require('../config')
 
 // Routes for authentication (signup, login, logout)
 module.exports = function (app, passport) {
 
-  const cas = new CASAuthentication({
-    cas_url: 'https://cas.utc.fr/cas/',
-    cas_version: '2.0',
-    service_url: 'http://localhost:3651',
-    session_info: 'cas_infos'
-  });
+  //set up the sessions to be stored in cookies for 2 hours
+  let expiryDate = new Date(Date.now() + 120 * 60 * 1000) // 2 hour
+  app.use(cookieSession({
+    name: 'cas_infos',
+    keys: ["Fillon", "rend", "largent"],
+    // Cookie Options
+    maxAge: expiryDate
+  }))
 
+  //CAS OBject to precise which CAS to use and how to call the session.
+  const cas = new CASAuthentication({
+    cas_url, // URL of UTC CAS 
+    cas_version, // CAS VErsion. 2 for UTC
+    service_url, //your URL
+    session_info: 'cas_infos' //name for the session send back by the CAS
+  })
+
+  //When "Connect" is clicked, redirect to CAS (bounce) and then to Index with the new session
   app.get('/login', cas.bounce, (req, res, next) => {
     if (requireLogin(req)) {
       // user is logged in
@@ -26,15 +41,16 @@ module.exports = function (app, passport) {
     }
   })
 
-  app.get('/logout', function (req, res, next) {
+  //When disconnect is clicked, just delete the session
+  app.get('/logout', (req, res, next) => {
     req.session = null
-    res.render('index', {
-      loggedIn: requireLogin(req)
-    })
+    res.redirect('index')
   })
 
+  //When the user consult his profile
   app.get('/profile', (req, res, next) => {
     if (requireLogin(req)) {
+      //the user is logged in. Get and display his infos
       casSession = req.session.cas_infos
       console.log(JSON.stringify("[profile.js]CasSession : " + JSON.stringify(casSession)))
       res.render('profile', {
@@ -44,9 +60,9 @@ module.exports = function (app, passport) {
         loggedIn: requireLogin(req)
       })
     } else {
-      // User is not authentified
+      // The user is not logged in. Rederiction to connection page.
       console.log("not logged in")
-      res.render('login', {
+      res.redirect('login', {
         loggedIn: requireLogin(req),
       })
     }
